57b47d985bb1a8b2bd18c966eb8020685c99de3c
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-04 00:28:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,13 @@
 # Create your costume_stores migration here
 class CreateYourCostumesStores < ActiveRevords::Migration[5.2]
-  
+  def change 
+    create_table :costumes do |t|
+      t.string :name 
+      t.integer :price
+      t.string :size 
+      t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
+  end
 end
